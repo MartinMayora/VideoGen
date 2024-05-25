656c2a1e27b4d9b9b30c119c53a28234d8ff783a
Martin Mayora <martinm@LAPTOP-27LB2UBB>
2024-05-25 15:11:29 -0300

    Change

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ glfwMakeContextCurrent(window);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, frame_width, frame_height, 0, GL_RGB, GL_UNSIGNED_BYTE,
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, frame_width, frame_height, 0, GL_RGBA, GL_UNSIGNED_BYTE,
                frame_data);
 
   while (!glfwWindowShouldClose(window)) {
